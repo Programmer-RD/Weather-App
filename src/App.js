d3253691a20a05c9469fc3a7c19f0c27a0c75eda
RanugaD <ranugagamage@gmail.com>
2020-11-15 10:13:30 +0530

    made the basic foundation of the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
-import "./App.css";
-
+const api_info = {
+  key: "d6311a70a2ccc2353bc5d14456aa0597",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 function App() {
-  return <div className="App"></div>;
+  const dateBuilder = (date) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    let today_date = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+    return `${day} ${today_date} ${month} ${year}`;
+  };
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div>
+          <div className="location-box">
+            <div className="location">Colombo,SL</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15 c
+          </div>
+          <div className="weather">
+            Sunny
+          </div>
+        </div>
+      </main>
+      <div className="footer">
+        <h3>
+          <b>Copyright © 2020 RanugaD</b>
+        </h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
